1c8e1191a2499f7180945ce24a0b70a88776a07d
Edmarnunes807 <edmarrocha807@gmail.com>
2025-09-14 19:57:57 -0400

    versão 1.1 beta

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Configurações fáceis de mudar
 const PIX_KEY = "69992402943"; // sua chave PIX
-const SHEET_URL = "https://script.google.com/macros/s/AKfycbxpLwO6QsidkTCGc6T5Ic4mLYyp4s3HB3psQPKp7HVAu6U_PqsVolSjYOSvbOn6EJubEw/exec"; // URL do Google Apps Script
+const SHEET_URL = "https://script.google.com/macros/s/AKfycbwsDACbs3K14MuvYfd57Fi-dRNGqwGkfoHMjaJtH64bm5dSRDlia79RaA93EP3RGyXUgA/exec"; // URL do Google Apps Script
