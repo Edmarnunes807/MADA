c4529adc2405f4738e5db7f14eca0d72a5e028a1
Edmarnunes807 <edmarrocha807@gmail.com>
2025-09-15 13:19:20 -0400

    versão 1.10 beta

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // config.js - apenas a URL do Apps Script (Web App) publicado
 // EXEMPLO: const SHEET_ENDPOINT = "https://script.google.com/macros/s/XXXXXXXX/exec";
-const SHEET_ENDPOINT = "https://script.google.com/macros/s/AKfycbznOvLlu-22or4N8UqqOBFc-XksKDD2acN54vOsHNIBuGHfpE7QaeRmD_FEJauH2RUB/exec";
+const SHEET_ENDPOINT = "https://script.google.com/macros/s/AKfycbxL1bt0Dxr3kI3YZ2T4kPdNTIX3AJSkHTDBcrV1C2wt8VtPJ2p8CcWUw5k3BF0yqYds/exec";
