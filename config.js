ac1c163cad287349389a96141246daf12028e06e
Edmarnunes807 <edmarrocha807@gmail.com>
2025-09-15 20:05:00 -0400

    1.01

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // config.js - apenas a URL do Apps Script (Web App) publicado
 // EXEMPLO: const SHEET_ENDPOINT = "https://script.google.com/macros/s/XXXXXXXX/exec";
-const SHEET_ENDPOINT = "https://script.google.com/macros/s/AKfycbxL1bt0Dxr3kI3YZ2T4kPdNTIX3AJSkHTDBcrV1C2wt8VtPJ2p8CcWUw5k3BF0yqYds/exec";
+const SHEET_ENDPOINT = "https://script.google.com/macros/s/AKfycbx4Yb7he2aeCTbcCwIcOtf3tLXgWi4pdmu-_b9rS3pyBfAJ9OIUXjYJnjCfsFJ-nBgc/exec";
